6c9676958a210cbbe6981195876d26845ea37113
abdullharslan <abdullharslan@protonmail.com>
2024-11-13 12:04:04 +0300

    IEntity.cs yazıldı.

--- ProductManagementAPI/Core/Entities/Abstract/IEntity.cs
+++ ProductManagementAPI/Core/Entities/Abstract/IEntity.cs
@@ -1,5 +1,16 @@
 namespace ProductManagementAPI.Core.Entities.Abstract;
 
+/*
+ * IEntity arayüzü, tüm varlıklar için temel bir kimlik tanımlayıcısı (Id) sağlayan soyut bir yapıdır.
+ * Bu arayüz, veritabanı işlemlerinde ortak bir ID alanına sahip olması gereken tüm varlıkların birbiriyle uyumlu bir
+ * şekilde çalışabilmesini sağlar.
+ *
+ * Özellikler:
+ * - Id: Her varlığa özgü birincil kimlik tanımlayıcısı.
+ *
+ * Bu arayüz, diğer tüm varlık sınıflarında temel bir yapı olarak kullanılarak, veritabanı işlemlerinin bir standart
+ * çerçevesinde yönetilmesine olanak tanır.
+ */
 public interface IEntity
 {
     public int Id { get; set; }
