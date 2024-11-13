f87943689b41dd449d54fa042bd8eb7a404dd16e
abdullharslan <abdullharslan@protonmail.com>
2024-11-13 12:09:19 +0300

    IEntityRepository.cs yazıldı.

--- ProductManagementAPI/Core/Repositories/Abstract/IEntityRepository.cs
+++ ProductManagementAPI/Core/Repositories/Abstract/IEntityRepository.cs
@@ -1,6 +1,31 @@
+using ProductManagementAPI.Core.Entities.Abstract;
+
 namespace ProductManagementAPI.Core.Repositories.Abstract;
 
-public interface IEntityRepository
+/*
+ * IEntityRepository arayüzü, genel veri erişim işlemleri için kullanılan temel metotları tanımlar.
+T tipindeki sınıflara (varlık sınıfları) uygulandığında, CRUD (Oluşturma, Okuma, Güncelleme, Silme) işlemlerini
+ * standartlaştırır. Bu yapı, uygulamanın genel veri erişim katmanında tutarlılık sağlar.
+ *
+ * T Sınıfı Kısıtlamaları:
+ * - T: Bir varlık sınıfı olmalıdır (IEntity arayüzünden türemeli) ve bir parametresiz yapıcıya sahip olmalıdır.
+ *
+ * Metotlar:
+ * - GetAllAsync: Tüm varlıkları asenkron olarak döndürür.
+ * - GetByIdAsync: Belirtilen Id ile bir varlık döndürür.
+ * - AddAsync: Yeni bir varlık ekler.
+ * - UpdateAsync: Mevcut bir varlığı günceller.
+ * - DeleteAsync: Belirtilen Id ile bir varlığı siler.
+ * - ExistsAsync: Belirtilen Id ile bir varlığın var olup olmadığını kontrol eder.
+ *
+ * Bu arayüz, veri erişim operasyonlarının tutarlı bir şekilde yönetilmesini sağlamak amacıyla yapılandırılmıştır.
+ */
+public interface IEntityRepository<T> where T : class, IEntity, new()
 {
-    
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+    Task<bool> ExistsAsync(int id);
 }
